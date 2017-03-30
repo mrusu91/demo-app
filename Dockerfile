343d211b46f9306cf8a49bb6a8ee8f390f84da13
Marian.Rusu <rusumarian91@gmail.com>
2017-03-30 15:25:27 +0100

    Release 1.2
    Signed-off-by: Marian.Rusu <rusumarian91@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,6 @@ ENV USER root
 WORKDIR /go/src/github.com/viruxel/demo-app
 COPY . .
 
+RUN go get -u github.com/lib/pq
+
 ENTRYPOINT ["/usr/local/go/bin/go", "run", "main.go"]
--- main.go
+++ main.go
@@ -1,20 +1,56 @@
 package main
 
 import (
+    "database/sql"
     "fmt"
     "net/http"
     "log"
+    "os"
+    _ "github.com/lib/pq"
+    "time"
 )
 
-func sayHelloWorld(w http.ResponseWriter, r *http.Request) {
+func sayhelloWorld(w http.ResponseWriter, r *http.Request) {
     log.Println("Responding to request")
-    fmt.Fprintf(w, "Hello World!\n")
+    DB_ADDR := os.Getenv("PG_ADDR")
+    DB_PORT := os.Getenv("PG_PORT")
+    DB_USER := os.Getenv("PG_USER")
+    DB_PASS := os.Getenv("PG_PASS")
+
+    dbinfo := fmt.Sprintf(
+      "host=%s port=%s user=%s password=%s sslmode=disable",
+      DB_ADDR,
+      DB_PORT,
+      DB_USER,
+      DB_PASS,
+    )
+    db, err := sql.Open("postgres", dbinfo)
+    if err != nil {
+        panic(err)
+    }
+    defer db.Close()
+
+    rows, err := db.Query("SELECT now()")
+    if err != nil {
+        panic(err)
+    }
+    for rows.Next() {
+        var now time.Time
+        err = rows.Scan(&now)
+        if err != nil {
+            panic(err)
+        }
+        fmt.Fprintf(w, "The time is: %s\n", now)
+    }
 }
 
 func main() {
-    log.Println("App started on port 8888")
-    panic("Let's crash :D")
-    http.HandleFunc("/", sayHelloWorld)
+    log.Println("PG_ADDR:", os.Getenv("PG_ADDR"))
+    log.Println("PG_PORT:", os.Getenv("PG_PORT"))
+    log.Println("PG_USER:", os.Getenv("PG_USER"))
+    log.Println("PG_PASS:", os.Getenv("PG_PASS"))
+
+    http.HandleFunc("/", sayhelloWorld)
     err := http.ListenAndServe(":8888", nil)
     if err != nil {
         log.Fatal(err)
